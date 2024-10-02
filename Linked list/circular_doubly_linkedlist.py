7928b100648f7a85f3e47a4b179bbf177d2c1b42
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2024-10-02 21:29:54 +0530

    circular doubly linked list

--- Linked list/circular_doubly_linkedlist.py
+++ Linked list/circular_doubly_linkedlist.py
@@ -30,17 +30,15 @@ class CircularLinkedList:
             self.head.next = new_value
             self.head.prev = new_value
             return True
-        temp = self.head
-        while temp.next != self.head:
-            temp = temp.next
+        temp = self.head.prev
         temp.next = new_value
         new_value.prev = temp
         new_value.next = self.head
         self.head.prev = new_value
 
     def insert_pos(self, pos, data):
-        if not self.head or pos < 1:
-            return False
+        if not self.head or pos < 1 or pos > self.length() + 1:
+            return False 
         if pos == 1:
             return self.insert_begin(data)
 
@@ -78,24 +76,60 @@ class CircularLinkedList:
             self.head = None
         return True
 
-    def delete_pos(self):
-        pass
+    def delete_data(self, data):
+        if not self.head:
+            return False
+        temp = self.head
+        if temp.data == data:
+            return self.delete_begin()
+        while temp.next != self.head:
+            if temp.data == data:
+                temp.prev.next = temp.next
+                temp.next.prev = temp.prev
+                return True
+            temp = temp.next
+        if temp.data == data:
+            temp.prev.next = self.head
+            self.head.prev = temp.prev
+            return True
+        else:
+            return False
 
     def length(self):
         if not self.head:
             return 0
-        count = 0
+        count = 1
         temp = self.head
         while temp.next != self.head:
             temp = temp.next
             count += 1
-        return count + 1
+        return count
 
     def reverse(self):
-        pass
+        if not self.head:
+            return False
+        temp = self.head
+        while temp.next != self.head:
+            next_val = temp.next
+            temp.next,temp.prev = temp.prev,temp.next
+            temp = next_val
+        temp.next,temp.prev = temp.prev,self.head
+        self.head = temp
 
-    def update_value(self):
-        pass
+    def update_value(self, data, new_data):
+        if not self.head:
+            return False
+        temp = self.head
+        while temp.next != self.head:
+            if temp.data == data:
+                temp.data = new_data
+                return True
+            temp = temp.next
+        if temp.data == data:
+            temp.data = new_data
+            return True
+        else:
+            return False
 
     def search(self, data):
         if not self.head:
@@ -133,3 +167,9 @@ circular_linkedlist.delete_begin()
 circular_linkedlist.traverse()
 print(circular_linkedlist.length())
 print(circular_linkedlist.search(2))
+print(circular_linkedlist.delete_data(9109))
+circular_linkedlist.traverse()
+circular_linkedlist.update_value(78, 80)
+circular_linkedlist.traverse()
+circular_linkedlist.reverse()
+circular_linkedlist.traverse()
