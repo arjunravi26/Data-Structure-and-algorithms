d5b789375412ecbb587fb9d918d479df929459c1
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2024-10-06 17:04:20 +0530

    stack implementaion using array

---
+++ stack/stack_array.py
@@ -0,0 +1,57 @@
+# Array implementation of static stack
+class Stack:
+    def __init__(self, max_size) -> None:
+        self.pointer = -1
+        self.size = max_size
+        self.stack = [None] * max_size
+
+    def push(self, data):
+        if self.isFull():
+            raise OverflowError("Stack Overflow")
+        self.pointer += 1
+        self.stack[self.pointer] = data
+        return f"Successfully inserted {data}"
+
+    def pop(self):
+        if self.isEmpty():
+            raise IndexError("Stack Underflow")
+        popped_value = self.stack[self.pointer]
+        self.stack[self.pointer] = None
+        self.pointer -= 1
+        return popped_value
+
+    def isEmpty(self):
+        return self.pointer == -1
+
+    def isFull(self):
+        return self.pointer == self.size - 1
+
+    def peek(self):
+        if self.isEmpty():
+            return None
+        return self.stack[self.pointer]
+
+    def __str__(self) -> str:
+        return f"{self.stack[:self.pointer+1]}"
+
+
+stack = Stack(max_size=5)
+stack.push(10)
+stack.push(10)
+stack.push(10)
+stack.push(10)
+stack.push(10)
+print(stack)
+stack.push(3)
+stack.push(0)
+
+print(stack)
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+print(stack)
+stack.pop()
+print(stack)
+print(stack.peek())
